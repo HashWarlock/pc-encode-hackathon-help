8e7eb5f67c4878668e2e81be961dfed11b425fb5
Charles LADARI <celadari@users.noreply.github.com>
2024-03-10 19:43:46 +0200

    fetch data from mongodb

--- contracts/oh_my_chess/src/lib.rs
+++ contracts/oh_my_chess/src/lib.rs
@@ -3,26 +3,27 @@ extern crate alloc;
 
 use pink_extension as pink;
 
+
 #[pink::contract(env=PinkEnvironment)]
 mod oh_my_chess {
-    use super::{oh_my_chess, pink};
+    use super::{pink};
     use pink::PinkEnvironment;
     use scale::{Encode, Decode};
-    use serde::Deserialize;
+    use serde::{Deserialize, Serialize};
     use alloc::string::String;
     use alloc::format;
-    // you have to use crates with `no_std` support in contract.
     use serde_json_core;
-    // use mongodb::{Client, options::ClientOptions};
+    use crate::oh_my_chess::Error::{NoElementFoundInDB, ErrorFetchingFromDB};
+    use scale_info::TypeInfo;
 
-    // use alloc::collections::BTreeMap;
-    // pub type GameId = u64;
-    // Type alias for the contract's result type.
+
     #[derive(Debug, PartialEq, Eq, Encode, Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
         InvalidMove,
         NotYourTurn,
+        NoElementFoundInDB,
+        ErrorFetchingFromDB,
     }
     pub type Result<T> = core::result::Result<T, Error>;
     pub type Option<T> = core::option::Option<T>;
@@ -32,6 +33,7 @@ mod oh_my_chess {
     pub struct OhMyChess {
         admin: AccountId,
         url: String,
+        api_key: String,
     }
 
     impl OhMyChess {
@@ -40,7 +42,8 @@ mod oh_my_chess {
         pub fn new() -> Self {
             Self {
                 admin: Self::env().caller(),
-                url: String::default()
+                url: String::from(""),
+                api_key: String::from(""),
             }
         }
 
@@ -54,19 +57,55 @@ mod oh_my_chess {
             self.url = url.clone();
         }
 
+        // #[ink(message)]
+        // pub fn make_move(&self, chess_move: ChessMove, player_to_move: Player) -> Result<bool, Error> {
+        //     let game_state = self.find_game_session_from_mongodb()?;
+        //
+        //     // let is_valid_move = self.check_move_validity(&chess_move, &game_state);
+        // }
+
         #[ink(message)]
-        pub fn make_move(&self, chess_move: ChessMove, player_to_move: Player) {
-            // let is_valid_move = self.check_move_validity(&chess_move, &player_to_move);
+        pub fn find_game_session_from_mongodb(&self, session_id: String) -> Result<GameState> {
+            let method = String::from("POST"); // HTTP Method for the request
+            let url = format!("{}/action/findOne?_id={}", self.url, session_id);
+
+            let data = r#"{
+                "collection":"game_sessions",
+                "database":"hackathon",
+                "dataSource":"Cluster0",
+                "projection":{"_id":0,"turn":1,"status":1,"players":1,"board":1}
+            }"#.as_bytes().to_vec();
+
+            // Prepare headers
+            let headers = alloc::vec![
+                (String::from("Content-Type"), String::from("application/json")),
+                (String::from("Access-Control-Request-Headers"), String::from("*")),
+                (String::from("api-key"), self.api_key.clone()),
+            ];
+
+            let response = pink::http_req!(
+                method,
+                url,
+                data,
+                headers
+            );
+
+            let game_state_res = serde_json_core::from_slice::<MongoDBDocument>(response.body.as_slice())
+                .map_err(|_| { ErrorFetchingFromDB })
+                .map(|(mongodb, _)| { mongodb.document })?
+                .ok_or(NoElementFoundInDB);
+
+            game_state_res
         }
 
         pub fn check_move_validity(&self, chess_move: &ChessMove, player_to_move: &Player, game_state: &GameState) -> bool {
             // Check if the move is within the board boundaries
-            if chess_move.from.0 < 0 || chess_move.from.0 > 7 || chess_move.from.1 < 0 || chess_move.from.1 > 7 || chess_move.to.0 < 0 || chess_move.to.0 > 7 || chess_move.to.1 < 0 || chess_move.to.1 > 7 {
+            if chess_move.from.0 > 7 || chess_move.from.1 > 7 || chess_move.to.0 > 7 || chess_move.to.1 > 7 {
                 return false;
             }
 
-            let (piece, player) = match game_state.board[chess_move.from.0 as usize][chess_move.from.1 as usize] {
-                Some(ref tuple) => tuple,
+            let ChessCell{ player, piece } = match game_state.board[chess_move.from.0 as usize][chess_move.from.1 as usize] {
+                Some(ref chess_cell) => chess_cell,
                 None => return false, // No piece at source
             };
 
@@ -97,13 +136,13 @@ mod oh_my_chess {
 
             // Check forward move
             if fx as i32 + forward == tx as i32 && fy == ty {
-                return game_state.board[tx][ty].is_none();
+                return game_state.board[tx as usize][ty as usize].is_none();
             }
 
             // Check capture move
             if fx as i32 + forward == tx as i32 && (fy as i32 - 1 == ty as i32 || fy as i32 + 1 == ty as i32) {
-                if let Some((_, piece_player)) = &game_state.board[tx as usize][ty as usize] {
-                    return *player != piece_player; // Capture if it's an opponent's piece
+                if let Some(ChessCell{player: piece_player, ..}) = &game_state.board[tx as usize][ty as usize] {
+                    return *player != *piece_player; // Capture if it's an opponent's piece
                 }
             }
 
@@ -117,14 +156,14 @@ mod oh_my_chess {
 
             // Check L-shape move
             if (dx == 2 && dy == 1) || (dx == 1 && dy == 2) {
-                if let Some((_, piece_player)) = &game_state.board[tx as usize][ty as usize] {
+                if let Some(ChessCell{player: piece_player, ..}) = &game_state.board[tx as usize][ty as usize] {
                     return *player != *piece_player; // Capture if there's an opponent piece
                 } else { true }
             } else { false }
 
         }
 
-        pub fn check_bishop_move_validity(&self, chess_move: &crate::oh_my_chess::ChessMove, game_state: &GameState) -> bool {
+        pub fn check_bishop_move_validity(&self, chess_move: &ChessMove, game_state: &GameState) -> bool {
             // Bishop can move diagonally
             let is_diagonal = (chess_move.from.0 as i32 - chess_move.to.0 as i32).abs() == (chess_move.from.1 as i32 - chess_move.to.1 as i32).abs();
 
@@ -179,7 +218,7 @@ mod oh_my_chess {
             }
         }
 
-        fn is_path_clear(&self, board: &[[Option<(Piece, Player)>; 8]; 8], chess_move: &ChessMove, direction: &Direction) -> bool {
+        fn is_path_clear(&self, board: &[[Option<ChessCell>; 8]; 8], chess_move: &ChessMove, direction: &Direction) -> bool {
             let from = chess_move.from;
             let to = chess_move.to;
             let (dx, dy) = (to.0 as i32 - from.0 as i32, to.1 as i32 - from.1 as i32);
@@ -214,47 +253,64 @@ mod oh_my_chess {
         }
     }
 
-    #[derive(Encode, Decode, Clone, Debug, PartialEq)]
+    #[derive(Encode, Decode, Deserialize, Serialize, Clone, Debug, PartialEq, TypeInfo)]
     pub enum Direction {
         Horizontal,
         Vertical,
         Diagonal,
     }
 
-    #[derive(Encode, Decode, Clone, Debug, PartialEq)]
+    #[derive(Encode, Decode, Deserialize, Serialize, Clone, Debug, PartialEq, TypeInfo)]
     pub enum Piece {
         Pawn, Knight, Bishop, Rook, Queen, King
     }
 
-    #[derive(Encode, Decode, Clone, Debug, PartialEq)]
+    #[derive(Encode, Decode, Deserialize, Serialize, Clone, Debug, PartialEq, TypeInfo)]
     pub enum Player {
         Black, White
     }
 
-    #[derive(Encode, Decode, Clone, Debug, PartialEq)]
+    #[derive(Encode, Decode, Deserialize, Serialize, Clone, Debug, PartialEq, TypeInfo)]
+    pub struct PlayersAddresses {
+        black: [u8; 32],
+        white: [u8; 32],
+    }
+
+    #[derive(Encode, Decode, Deserialize, Serialize, Clone, Debug, PartialEq, TypeInfo)]
     pub enum GameStatus {
         Ongoing, Checkmate, Stalemate, Draw
     }
 
-    #[derive(Encode, Decode, Clone, Debug)]
+    #[derive(Encode, Decode, Deserialize, Serialize, Clone, Debug, TypeInfo)]
     pub struct GameState {
-        board: [[Option<(Piece, Player)>; 8]; 8],
+        board: [[Option<ChessCell>; 8]; 8],
         turn: Player,
-        players: (AccountId, AccountId),
+        players: PlayersAddresses,
         status: GameStatus,
     }
 
-    #[derive(Encode, Decode, Clone, Debug, PartialEq)]
+    #[derive(Encode, Decode, Deserialize, Serialize, Clone, Debug, PartialEq, TypeInfo)]
+    pub struct ChessCell {
+        piece: Piece,
+        player: Player,
+    }
+
+    #[derive(Encode, Decode, Deserialize, Serialize, Clone, Debug, PartialEq, TypeInfo)]
     pub struct ChessMove {
         // Define your move structure
         from: (u8, u8),
         to: (u8, u8),
     }
 
+    #[derive(Encode, Decode, Deserialize, Clone, Debug)]
+    pub struct MongoDBDocument {
+        // Define your move structure
+        document: Option<GameState>
+    }
+
     #[cfg(test)]
     mod tests {
-        use serde::__private::de::Content::String;
-        /// Imports all the definitions from the outer scope so we can use them here.
+        /// Imports all the definitions from the outer scope, so we can use them here.
         use super::*;
 
         /// We test a simple use case of our contract.
