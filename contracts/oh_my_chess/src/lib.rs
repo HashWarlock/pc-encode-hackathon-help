c064d1e0d31b1c8333cb7ff5d21cc626fb7add62
Charles LADARI <celadari@users.noreply.github.com>
2024-03-11 03:30:03 +0200

    finish almost

--- contracts/oh_my_chess/src/lib.rs
+++ contracts/oh_my_chess/src/lib.rs
@@ -9,11 +9,12 @@ mod oh_my_chess {
     use super::{pink};
     use pink::PinkEnvironment;
     use scale::{Encode, Decode};
+    use alloc::format;
     use serde::{Deserialize, Serialize};
     use alloc::string::String;
-    use alloc::format;
+    use pink_extension::{log, warn};
     use serde_json_core;
-    use crate::oh_my_chess::Error::{ToIsOccupiedByOneOfYourPiece, PieceSelectedIsNotYours, NoPieceBoardChessFrom, OutOfBoardChessFrom, OutOfBoardChessTo, NonValidMove, NoElementFoundInDB, ErrorFetchingFromDB, NotAuthorized, NotYourTurn, YourNotInThisGameSession};
+    use crate::oh_my_chess::Error::{WrongPlayerAddressArgument, ErrorInsertingToDB, CouldNotUpdateDB, ToIsOccupiedByOneOfYourPiece, PieceSelectedIsNotYours, NoPieceBoardChessFrom, OutOfBoardChessFrom, OutOfBoardChessTo, NonValidMove, NoElementFoundInDB, ErrorFetchingFromDB, NotAuthorized, NotYourTurn, YourNotInThisGameSession};
     use scale_info::TypeInfo;
 
 
@@ -24,13 +25,16 @@ mod oh_my_chess {
         NotYourTurn,
         YourNotInThisGameSession,
         NoElementFoundInDB,
+        CouldNotUpdateDB,
         ErrorFetchingFromDB,
+        ErrorInsertingToDB,
         NotAuthorized,
         OutOfBoardChessFrom,
         OutOfBoardChessTo,
         NoPieceBoardChessFrom,
         PieceSelectedIsNotYours,
         ToIsOccupiedByOneOfYourPiece,
+        WrongPlayerAddressArgument,
     }
     pub type Result<T> = core::result::Result<T, Error>;
     pub type Option<T> = core::option::Option<T>;
@@ -46,12 +50,66 @@ mod oh_my_chess {
     impl OhMyChess {
 
         #[ink(constructor)]
-        pub fn new() -> Self {
-            Self {
+        pub fn new() -> Result<Self> {
+            let instance = Self {
                 admin: Self::env().caller(),
-                url: String::from(""),
-                api_key: String::from(""),
-            }
+                url: String::from("https://eu-central-1.aws.data.mongodb-api.com/app/data-bluba/endpoint/data/v1"),
+                api_key: String::from("Xq8d8xug4fxn9mz9JERMR4D72GhDRn5M2SUK49G4qjJ2FlaXd0NPptKLzPoyK9bN"),
+            };
+            Ok(instance)
+        }
+
+        #[ink(message)]
+        pub fn start_new_game_session(&self) -> Result<String> {
+            let player_black: [u8; 32] = *(Self::env().caller().as_ref());
+            let player_white: [u8; 32] = *(Self::env().caller().as_ref());
+
+            let initial_board = [
+                [ // 1st rank (from White's perspective)
+                    Some(ChessCell { piece: Piece::Rook, player: Player::White }),
+                    Some(ChessCell { piece: Piece::Knight, player: Player::White }),
+                    Some(ChessCell { piece: Piece::Bishop, player: Player::White }),
+                    Some(ChessCell { piece: Piece::Queen, player: Player::White }),
+                    Some(ChessCell { piece: Piece::King, player: Player::White }),
+                    Some(ChessCell { piece: Piece::Bishop, player: Player::White }),
+                    Some(ChessCell { piece: Piece::Knight, player: Player::White }),
+                    Some(ChessCell { piece: Piece::Rook, player: Player::White }),
+                ],
+                [ // 2nd rank
+                    Some(ChessCell { piece: Piece::Pawn, player: Player::White }); 8 // All pawns
+                ],
+                [None; 8], // 3rd rank
+                [None; 8], // 4th rank
+                [None; 8], // 5th rank
+                [None; 8], // 6th rank
+                [ // 7th rank
+                    Some(ChessCell { piece: Piece::Pawn, player: Player::Black }); 8 // All pawns
+                ],
+                [ // 8th rank (from Black's perspective)
+                    Some(ChessCell { piece: Piece::Rook, player: Player::Black }),
+                    Some(ChessCell { piece: Piece::Knight, player: Player::Black }),
+                    Some(ChessCell { piece: Piece::Bishop, player: Player::Black }),
+                    Some(ChessCell { piece: Piece::Queen, player: Player::Black }),
+                    Some(ChessCell { piece: Piece::King, player: Player::Black }),
+                    Some(ChessCell { piece: Piece::Bishop, player: Player::Black }),
+                    Some(ChessCell { piece: Piece::Knight, player: Player::Black }),
+                    Some(ChessCell { piece: Piece::Rook, player: Player::Black }),
+                ],
+            ];
+            let game_state = GameState {
+                board: initial_board,
+                turn: Player::White, // White starts the game
+                players: PlayersAddresses {
+                    black: player_black.clone(),
+                    white: player_white.clone(),
+                },
+                status: GameStatus::Ongoing,
+            };
+            let inserted_document_id = self.insert_game_session_to_mongodb(game_state)?;
+            self.update_players_sessions_track_in_mongodb(inserted_document_id.clone(), player_black)?;
+            self.update_players_sessions_track_in_mongodb(inserted_document_id.clone(), player_white)?;
+
+            Ok(String::from(inserted_document_id.as_str()))
         }
 
         pub fn is_admin(&self) -> bool {
@@ -89,20 +147,58 @@ mod oh_my_chess {
         }
 
         #[ink(message)]
-        pub fn make_move(&mut self, chess_move: ChessMove) -> Result<GameState> {
-            let mut game_state = self.find_game_session_from_mongodb()?;
+        pub fn make_move(&self) -> Result<()> {
+            let chess_move: ChessMove = ChessMove{ from: (1, 2), to: (3, 2)};
+            let session_id: String = String::from("65ee5d8a95f3de8dcf8d091f");
+            let mut game_state = self.find_game_session_from_mongodb(session_id.clone())?;
 
+            // check validity of the move
             Self::check_move_boundaries(&chess_move)?;
             Self::check_caller_turn(&game_state)?;
             Self::check_caller_owns_piece(&game_state, &chess_move)?;
             Self::check_move_validity_for_piece(&game_state, &chess_move)?;
 
+            // update game_state: board, status
+            Self::update_board_from_move(&mut game_state, &chess_move)?;
+            Self::update_turn_and_status(&mut game_state, &chess_move)?;
+
+            // update mongodb
+            self.update_game_session_to_mongodb(game_state, session_id)
+        }
+
+        fn update_turn_and_status(game_state: &mut GameState, chess_move: &ChessMove) -> Result<()> {
+            let (tx, ty) = chess_move.to;
+            if let Some(ChessCell { piece: Piece::King, .. }) = game_state.board[tx as usize][ty as usize] {
+                game_state.status = GameStatus::Finished;
+            } else {
+                game_state.turn = match game_state.turn {
+                    Player::Black => Player::White,
+                    Player::White => Player::Black,
+                };
+            }
+            Ok(())
+        }
+
+        fn update_board_from_move(game_state: &mut GameState, chess_move: &ChessMove) -> Result<()> {
             let (fx, fy) = chess_move.from;
             let (tx, ty) = chess_move.to;
-            let chess_cell_option = &game_state.board[fx as usize][fy as usize];
-            game_state.board[fx as usize][fy as usize] = Some((*chess_cell_option).clone().unwrap());
-            game_state.board[tx as usize][ty as usize] = None;
-            Ok(game_state)
+
+            let chess_cell_option = game_state.board[fx as usize][fy as usize].take(); // Directly take the value out
+            if let Some(ChessCell { piece: Piece::Pawn, .. }) = &chess_cell_option {
+                // Check if the pawn reaches the end and should be promoted to a queen
+                if tx == 7 || tx == 0 {
+                    game_state.board[tx as usize][ty as usize] = Some(ChessCell { piece: Piece::Queen, player: game_state.turn });
+                } else {
+                    game_state.board[tx as usize][ty as usize] = chess_cell_option;
+                }
+            } else {
+                game_state.board[tx as usize][ty as usize] = chess_cell_option;
+            }
+
+            // Clear the original location after moving the piece
+            game_state.board[fx as usize][fy as usize] = None;
+
+            Ok(())
         }
 
         pub fn check_caller_owns_piece(game_state: &GameState, chess_move: &ChessMove) -> Result<()> {
@@ -142,8 +238,7 @@ mod oh_my_chess {
         }
 
         #[ink(message)]
-        pub fn find_game_session_from_mongodb(&self) -> Result<GameState> {
-            let session_id = String::from("65ede971e64bb9047421d3b3");
+        pub fn find_game_session_from_mongodb(&self, session_id: String) -> Result<GameState> {
             let method = String::from("POST"); // HTTP Method for the request
             let url = format!("{}/action/findOne?_id={}", self.url, session_id);
 
@@ -168,12 +263,162 @@ mod oh_my_chess {
                 headers
             );
 
-            let game_state_res = serde_json_core::from_slice::<MongoDBDocument>(response.body.as_slice())
+            let game_state_res = serde_json_core::from_slice::<FindMongoDBDocumentResult>(response.body.as_slice())
                 .map_err(|_| { ErrorFetchingFromDB })
                 .map(|(mongodb, _)| { mongodb.document })?
                 .ok_or(NoElementFoundInDB);
 
             game_state_res
+        }
+
+        pub fn update_players_sessions_track_in_mongodb(&self, session_id: heapless::String<32>, player_address: [u8; 32]) -> Result<()> {
+            let method = String::from("POST"); // HTTP Method for the request
+            let player_address_hex_string = Self::bytes_to_hex_string(player_address)?;
+            let url = format!("{}/action/updateOne?_id={}", self.url, player_address_hex_string);
+
+            pink::warn!("-----SESSION_ID FORMAT {}", session_id.clone());
+            pink::warn!("-----URL FORMAT {}", url.clone());
+
+            let data = format!(r#"{{
+                "collection":"players_sessions_trackers",
+                "database":"hackathon",
+                "dataSource":"Cluster0",
+                "filter":{{}},
+                "update":{{"$push": {{"sessions": "{}"}} }},
+                "upsert":true
+            }}"#, session_id);
+            // pink::warn!("-----DATA FORMAT {}", data);
+
+            let data = format!(r#"{{
+                "collection":"players_sessions_trackers",
+                "database":"hackathon",
+                "dataSource":"Cluster0",
+                "filter":{{}},
+                "update":{{"$push": {{"sessions": "{}"}} }},
+                "upsert":true
+            }}"#, session_id).as_bytes().to_vec();
+
+            // let data_format: core::result::Result<heapless::String<8192>, _> = serde_json_core::ser::to_string(data.as_slice());
+            // pink::warn!("-----UPDATE DATA FORMAT\n {:?}", data_format);
+
+            // Prepare headers
+            let headers = alloc::vec![
+                (String::from("Content-Type"), String::from("application/json")),
+                (String::from("Access-Control-Request-Headers"), String::from("*")),
+                (String::from("api-key"), self.api_key.clone()),
+            ];
+
+            let response = pink::http_req!(
+                method,
+                url,
+                data,
+                headers
+            );
+
+            if response.status_code == 200 { Ok(()) }
+            else { Err(CouldNotUpdateDB) }
+        }
+
+        pub fn update_game_session_to_mongodb(&self, game_state: GameState, session_id: String) -> Result<()> {
+            let method = String::from("POST"); // HTTP Method for the request
+            let url = format!("{}/action/updateOne?_id={}", self.url, session_id);
+
+            let json_game_state: heapless::String<8192> = serde_json_core::ser::to_string(&game_state).map_err(|_| { CouldNotUpdateDB })?;
+
+            let data = format!(r#"{{
+                "collection":"game_sessions",
+                "database":"hackathon",
+                "dataSource":"Cluster0",
+                "filter":{{}},
+                "update":{{"$set": {}}}
+            }}"#, json_game_state);
+            pink::warn!("-----DATA FORMAT {}", data);
+
+            let data = format!(r#"{{
+                "collection":"game_sessions",
+                "database":"hackathon",
+                "dataSource":"Cluster0",
+                "filter":{{}},
+                "update":{{"$set": {}}}
+            }}"#, json_game_state).as_bytes().to_vec();
+
+
+
+            // Prepare headers
+            let headers = alloc::vec![
+                (String::from("Content-Type"), String::from("application/json")),
+                (String::from("Access-Control-Request-Headers"), String::from("*")),
+                (String::from("api-key"), self.api_key.clone()),
+            ];
+
+            let response = pink::http_req!(
+                method,
+                url,
+                data,
+                headers
+            );
+
+            if response.status_code == 200 { Ok(()) }
+            else { Err(CouldNotUpdateDB) }
+        }
+
+        pub fn insert_game_session_to_mongodb(&self, game_state: GameState) -> Result<heapless::String<32>> {
+            let method = String::from("POST"); // HTTP Method for the request
+            let url = format!("{}/action/insertOne", self.url);
+
+            let json_game_state: heapless::String<4096> = serde_json_core::ser::to_string(&game_state).map_err(|_| { ErrorInsertingToDB })?;
+            // pink::warn!("#####////\n{:?}", json_game_state);
+
+            let data = format!(r#"{{
+                "collection":"game_sessions",
+                "database":"hackathon",
+                "dataSource":"Cluster0",
+                "document":{}
+            }}"#, json_game_state).as_bytes().to_vec();
+
+
+            // Prepare headers
+            let headers = alloc::vec![
+                (String::from("Content-Type"), String::from("application/json")),
+                (String::from("Access-Control-Request-Headers"), String::from("*")),
+                (String::from("api-key"), self.api_key.clone()),
+            ];
+
+            let response = pink::http_req!(
+                method,
+                url,
+                data.clone(),
+                headers
+            );
+
+            // pink::warn!("-----RESPONSE\n {:?}", response.body.clone());
+            // let data_format: core::result::Result<heapless::String<8192>, _> = serde_json_core::ser::to_string(data.as_slice());
+            // pink::warn!("-----DATA FORMAT\n {:?}", data_format);
+
+            // let response_format: core::result::Result<heapless::String<1024>, _> = serde_json_core::ser::to_string(response.body.clone().as_slice());
+            // pink::warn!("-----RESPONSE FORMAT\n {:?}", response_format);
+
+            serde_json_core::from_slice::<InsertedMongoDBDocumentResult>(response.body.as_slice())
+                .map_err(|_| { ErrorInsertingToDB })
+                .map(|(inserted_document_mongo, _)| { inserted_document_mongo.insertedId })?
+                .ok_or(ErrorInsertingToDB)
+        }
+
+        pub fn bytes_to_hex_string(bytes: [u8; 32]) -> Result<heapless::String<95>> {
+            let mut s: heapless::String<95> = heapless::String::new(); // Adjust the size accordingly
+
+            for (i, byte) in bytes.iter().enumerate() {
+                // Write each byte as hex
+                // Note: heapless::String does not support format! macro
+                s.push_str(&format!("{:02x}", byte)).map_err(|_| { WrongPlayerAddressArgument })?; // Push each byte as a hex string
+
+                // Don't add a '-' after the last byte
+                if i < bytes.len() - 1 {
+                    s.push('-').map_err(|_| { WrongPlayerAddressArgument })?; // Separate bytes with a '-'
+                }
+            }
+
+            Ok(s)
         }
 
         pub fn check_move_boundaries(chess_move: &ChessMove) -> Result<()> {
@@ -212,8 +457,16 @@ mod oh_my_chess {
                 Player::Black => -1,
             };
 
-            // Check forward move
+            // Check forward move of 1 step
             if fx as i32 + forward == tx as i32 && fy == ty && game_state.board[tx as usize][ty as usize].is_none() {
+                return Ok(());
+            }
+
+            // Check forward move of 2 steps
+            if ((player == &Player::White && fx == 1) || (player == &Player::Black && fx == 6))
+                && tx as i32 == fx as i32 + 2 * forward && fy == ty && game_state.board[tx as usize][ty as usize].is_none()
+                && game_state.board[(fx as i32 + forward) as usize][fy as usize].is_none() {
+                // Check if the path is clear
                 return Ok(());
             }
 
@@ -340,12 +593,12 @@ mod oh_my_chess {
         Diagonal,
     }
 
-    #[derive(Encode, Decode, Deserialize, Serialize, Clone, Debug, PartialEq, TypeInfo)]
+    #[derive(Encode, Decode, Deserialize, Serialize, Copy, Clone, Debug, PartialEq, TypeInfo)]
     pub enum Piece {
         Pawn, Knight, Bishop, Rook, Queen, King
     }
 
-    #[derive(Encode, Decode, Deserialize, Serialize, Clone, Debug, PartialEq, TypeInfo)]
+    #[derive(Encode, Decode, Deserialize, Serialize, Copy, Clone, Debug, PartialEq, TypeInfo)]
     pub enum Player {
         Black, White
     }
@@ -358,7 +611,7 @@ mod oh_my_chess {
 
     #[derive(Encode, Decode, Deserialize, Serialize, Clone, Debug, PartialEq, TypeInfo)]
     pub enum GameStatus {
-        Ongoing, Checkmate, Stalemate, Draw
+        Ongoing, Finished, Stalemate, Draw
     }
 
     #[derive(Encode, Decode, Deserialize, Serialize, Clone, Debug, TypeInfo)]
@@ -369,7 +622,7 @@ mod oh_my_chess {
         status: GameStatus,
     }
 
-    #[derive(Encode, Decode, Deserialize, Serialize, Clone, Debug, PartialEq, TypeInfo)]
+    #[derive(Encode, Decode, Deserialize, Serialize, Copy, Clone, Debug, PartialEq, TypeInfo)]
     pub struct ChessCell {
         piece: Piece,
         player: Player,
@@ -382,26 +635,32 @@ mod oh_my_chess {
     }
 
     #[derive(Encode, Decode, Deserialize, Clone, Debug)]
-    pub struct MongoDBDocument {
+    pub struct FindMongoDBDocumentResult {
         document: Option<GameState>
     }
 
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope, so we can use them here.
-        use super::*;
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            // when your contract is really deployed, the Phala Worker will do the HTTP requests
-            // mock is needed for local test
-            pink_extension_runtime::mock_ext::mock_all_ext();
-
-            let oh_my_chess = OhMyChess::new();
-            assert!(oh_my_chess.get_url(), "");
-
-        }
+    #[allow(non_snake_case)]
+    #[derive(Deserialize, Clone, Debug)]
+    pub struct InsertedMongoDBDocumentResult {
+        insertedId: Option<heapless::String<32>>
     }
+
+    // #[cfg(test)]
+    // mod tests {
+    //     /// Imports all the definitions from the outer scope, so we can use them here.
+    //     use super::*;
+
+        // We test a simple use case of our contract.
+        // #[ink::test]
+        // fn it_works() {
+        //     // when your contract is really deployed, the Phala Worker will do the HTTP requests
+        //     // mock is needed for local test
+        //     pink_extension_runtime::mock_ext::mock_all_ext();
+        //
+        //     let oh_my_chess = OhMyChess::new();
+        //     assert!(oh_my_chess.get_url(), "");
+        //
+        // }
+    // }
 
 }
